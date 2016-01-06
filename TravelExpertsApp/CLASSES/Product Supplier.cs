953382b701398c6ad23e66f8bc9ded68e2112d5a
SPYcy <alphamariox@gmail.com>
2016-01-06 15:45:27 -0700

    Addition of Classes
    +Added classes into a new directory.

---
+++ TravelExpertsApp/CLASSES/Product Supplier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team5_OOSD_Fall2015_Project2
+{
+    class Product_Supplier
+    {
+        //Class attributes.
+        //private int productSupplierId;
+        //private int productId;
+        //private int supplierId;
+
+        //Class properties.
+        public int ProductSupplierId { get; set; }
+        public int ProductId { get; set; }
+        public int SupplierId { get; set; }
+
+    }
+}
