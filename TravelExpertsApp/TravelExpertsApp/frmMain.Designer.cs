c747aec783ce5be282e59152c4fd693b9b2e0f8c
SPYcy <alphamariox@gmail.com>
2016-01-06 16:09:24 -0700

    Minor Aesthetic Changes
    "See for yourself."

--- TravelExpertsApp/TravelExpertsApp/frmMain.Designer.cs
+++ TravelExpertsApp/TravelExpertsApp/frmMain.Designer.cs
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1033, 522);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Travel Experts Package Editor";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
