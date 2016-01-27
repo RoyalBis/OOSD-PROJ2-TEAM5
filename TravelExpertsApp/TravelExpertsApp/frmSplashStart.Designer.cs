6c7b3f4f0892b297eaa103239a23ad4a65e9ac86
Riitard <alphamariox@gmail.com>
2016-01-27 13:27:24 -0700

    Splash Page
    I hope you like my weak sense of humor.

---
+++ TravelExpertsApp/TravelExpertsApp/frmSplashStart.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TravelExpertsApp
+{
+    partial class frmSplashStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSplashStart));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.tmStart = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(702, 454);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.Location = new System.Drawing.Point(210, 484);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(310, 48);
+            this.lblStart.TabIndex = 1;
+            this.lblStart.Text = "Loading system components.\r\nConnecting to database.";
+            // 
+            // tmStart
+            // 
+            this.tmStart.Tick += new System.EventHandler(this.tmStart_Tick);
+            // 
+            // frmSplashStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(727, 567);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmSplashStart";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frmSplashStart_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Timer tmStart;
+    }
+}
---
+++ TravelExpertsApp/TravelExpertsApp/frmSplashStart.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TravelExpertsApp
+{
+    public partial class frmSplashStart : Form
+    {
+        private int tLeft;
+        private SoundPlayer hoot = new SoundPlayer();
+        public frmSplashStart()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSplashStart_Load(object sender, EventArgs e)
+        {
+            //When the application starts, display this page.
+            tLeft = 5;
+            hoot.Play();
+            tmStart.Start();
+        }
+
+        private void tmStart_Tick(object sender, EventArgs e)
+        {
+            //When the timer ends, hide this splash page.
+            while (tLeft < 0)
+            {
+                tLeft -= tLeft;
+            }
+            tmStart.Stop();
+            //Loading form goes here.
+
+            hoot.Stop();
+            this.Hide();
+        }
+    }
+}
